b5054f37a37e4a4c2d971f115038a4bd32688536
سـورس مـاڪـس <120306685+music145@users.noreply.github.com>
2022-12-21 04:34:06 +0200

    Update id.py

--- YukkiMusic/plugins/tools/id.py
+++ YukkiMusic/plugins/tools/id.py
@@ -26,7 +26,7 @@ def get_id(msg: Message):
                 return obj
 
 
-@app.on_message(command(["ايدي","ا","الايدي"]))
+@app.on_message(command(["كشف","/id","id","ايدي","ا","الايدي"]))
 async def showid(_, message: Message):
     chat_type = message.chat.type
 
